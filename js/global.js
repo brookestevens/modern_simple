801c5c43806c81573fda101a642ce70ede64cc88
brookestevens <brst4163@colorado.edu>
2020-07-09 10:30:24 -0600

    updating theme

--- js/global.js
+++ js/global.js
@@ -1,10 +1,7 @@
 (function($){
     let nav = $('.region-primary-menu');
     let sticky = nav.offset().top;
-    
     window.addEventListener('scroll', () => {
-        console.log(nav);
-        console.log(sticky, this.pageYOffset);
         if(this.pageYOffset >= sticky){
             nav.addClass('sticky');
         }
